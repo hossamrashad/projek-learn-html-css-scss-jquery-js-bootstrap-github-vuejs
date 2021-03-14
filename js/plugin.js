e981de9f5291ce4d49e703a18040d7b82860c20f
hossamrashad <bibo2010508@yahoo.com>
2021-03-14 04:13:22 -0700

    inet

---
+++ js/plugin.js
@@ -0,0 +1,20 @@
+/* global document, console, alert */
+/**
+ *
+ *  innerHTML   هو انو العنصار المختار يساوى الحاجة اللى انت بتكتبها
+ *
+ *
+ *
+ */
+// some vers
+
+var //some vers
+    x = 10,
+    s = 15,
+    d = 20;
+document.getElementById("video5").innerHTML = x;
+if (x + s === 30) {
+    console.log("good");
+} else {
+    console.log("bad");
+}
