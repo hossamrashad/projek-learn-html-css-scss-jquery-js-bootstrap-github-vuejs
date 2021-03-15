55e324ab2d2b171a0520f3d13c4dadf59e29acbf
hossamrashad <bibo2010508@yahoo.com>
2021-03-15 04:35:21 -0700

    go

--- js/startJquery.js
+++ js/startJquery.js
@@ -21,3 +21,40 @@ $(document).ready(function () {
   "use strict";
   $("#jqvidero2 .jqvideo21").css("color", "#F00");
 });
+/****************************** */
+// video3
+// event click|| dblclick || mouseenter || moudeLeave || hover
+$(document).ready(function () {
+  "use strict";
+  $("#jqbtnvideo3").click(function () {
+    // this راجاع على العنصار اللى انا مختارو
+    $(this).css("backgroundColor", "#F00");
+    $("#jqpvideo3").hide();
+  });
+});
+$(document).ready(function () {
+  "use strict";
+  $("#jqbtnvideo31").dnlclick(function () {
+    $(this).css("backgroundColor", "#080");
+    $("#jqpvideo31").hide();
+  });
+});
+$(document).ready(function () {
+  "use strict";
+  $("#jqbtnvideo32").mouseenter(function () {
+    $(this).css("backgroundColor", "#080");
+    $("#jqpvideo32").hide();
+  });
+});
+$(document).ready(function () {
+  $("#jqbtnvideo33").mouseleave(function () {
+    $(this).css("backgroundColor", "#3498db");
+    $("#jqpvideo33").css("color", "#3498db");
+  });
+});
+$(document).ready(function () {
+  "use strict";
+  $("#jqbtnvideo34").hover(function () {
+    $(this).hide;
+  });
+});
