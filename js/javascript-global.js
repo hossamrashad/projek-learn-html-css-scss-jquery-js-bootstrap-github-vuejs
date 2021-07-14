94d0103d31d2593c5260d32a44fd7601a22529f4
hossamrashad <bibo2010508@yahoo.com>
2021-07-14 22:59:12 +0200

    go 14 7 2021

--- js/javascript-global.js
+++ js/javascript-global.js
@@ -33,13 +33,34 @@
 // todo console.dir(ElementName);
 // todo console.log(ElementName);
 
+// explain طباعة الداتا اللى فى العنصر و علشان تجيب نوع داتا بعينو بتكتب اسمو
+// -- div(id="Test" data-name="Hossam" data-parent="Rashad")
+// ++ var ElementName = document.querySelector("#Test");
+// todo console.log(ElementName.dataset);
+// todo console.log(ElementName.dataset.name);
+
+// explain طريقة عمال فى الكنسال جروب
+// todo console.group("Person Data");
+// todo console.log("Name: Hossam");
+// todo console.log("Age: 30");
+// todo console.groupEnd();
+
+// explain طريقة كتاب استرانج فى الكنسال بى الالوان
+// todo console.log("I am regular and %cI'm Hossam Rashad", "color: red; font-weight: bold;");
+
+// explain
+// todo console.time("addHeadings");
+// todo for (let i = 0; i < 10000; i++) {
+// todo  document.body.insertAdjacentHTML("beforeend", "<h5>Heading</h5>");
+// todo }
+// todo console.timeEnd("addHeadings");
 
 // =                                   javascript Dom
 
-// explain انشاء عنصر 
+// explain انشاء عنصر
 // 1 var ElementName = document.createElement("div");
 
-// explain اضافة العنصر فى المكان اللى انت عاوزة 
+// explain اضافة العنصر فى المكان اللى انت عاوزة
 // 2  var ElementName = document.body.appendChild(ElementName);
 
 // explain  انشاء تاكست
@@ -48,14 +69,35 @@
 // explain تغير الالتربيوت او انشاء لو مش موجود
 // 4  Element.setAttribute('class', 'value');
 
+// explain بيجيب جميع خواص العنصار 
+/**
+ * ++ Element.getBoundingClientRect();
+ * -- bottom: بيكون المسافات بين العنصار من فوق فى مساحة الصفحة لاخر العنصر من تحت 
+ * -- height: ارتفاع العنصار 
+ * -- left: المارجان اللى فى العنصار 
+ * -- right: بيحساب عرض العنصار و كمان بيضيف المارجان اللى على اليمين 
+ * -- top: المسافات اللى بين العنصار و اول الصفحة 
+ * -- width: عرض العنصر 
+ * -- x: اللى هو المارجان 
+ * -- y: المسافات اللى بين العنصار و اول الصفحة 
+ */
 
-// todo                                     if & elsewhere
+// explain الاوتار تاكست اللى هو العنصر زاى مهو مكتوب فى صفحة html 
+// 5 get outerHTML
+// 6 outSide.textContent = child.outerHTML;
+
+// 7 set outerHTML
+// 8 child.outerHTML = "<div class='child'>Element Outer Html  change</div>";
+
+// todo                                     if & else
 /*
 
 explain التاكاد من وجود الكلاس 
 
 var ElementName = document.querySelector("#HossamRashad");
+
 ++ الكلاس مش موجود  
+
 if (ElementName.classList.contains("explain")) {
     ElementName.classList.remove("explain");
     console.log(ElementName.classList);
@@ -65,38 +107,62 @@ if (ElementName.classList.contains("explain")) {
 }
 
 * *************************************************
-
 */
 
-/************************** */
+// todo                                     For Loop
 
-var ElementName = document.querySelector("#Test");
+/**
+ * 
+// -- get ul id
 
-if (ElementName.classList.contains("explain")) {
-    ElementName.classList.remove("explain");
-    console.log(ElementName.classList);
-} else {
-    ElementName.classList.add("explain");
-    console.log(ElementName.classList);
-}
+//  ++ var myList = document.getElementById("myList");
 
+// -- array name 
 
-var ElementNames = document.createElement("li");
+//  ++ var fruitList = ["Rovan", "Hossam", "Rashad"];
 
-ElementNames.textContent = "Rovan Hossam";
-console.dir(ElementName);
+// ** for loop 
 
-// get ul id
-
-var myList = document.getElementById("myList");
-
-// array name 
-var fruitList = ["Rovan", "Hossam", "Rashad"];
-
-// 
 for (let fruit of fruitList) {
     let newListItem = document.createElement("li");
     newListItem.textContent = fruit;
     myList.appendChild(newListItem);
 }
+
+ */
+
+// if nav have class "fixed-top"
+$(function() {
+    var nav = document.querySelector(".fixed-top");
+
+    if (nav.classList.contains("fixed-top")) {
+        var nextElementNav = nav.nextElementSibling;
+        nextElementNav.style.marginTop = nav.clientHeight + "px";
+        // console.log(nav.classList);
+    }
+});
+
+/************************** */
+
+var body = document.getElementsByTagName("body");
+
+var parent = document.querySelector("div.parent");
+var child = document.querySelector("div.child");
+var outSide = document.querySelector("div.outSide");
+
+var outerExplainOne = document.querySelector(".outerExplainOne");
+var outerExplainTow = document.querySelector(".outerExplainTow");
+
+var Last = document.querySelector("#Last");
+var Test = document.querySelector("#Test");
+var TestOne = document.querySelector("#TestOne");
+var TestTow = document.querySelector("#TestTow");
+var ulList = document.querySelector("#ulList");
+var liFirst = document.querySelector("#ulList > li:first-of-type");
+var addLi = document.createElement("li");
+
+
+
+
+
 /************************** */
